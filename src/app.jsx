e218c2a9a020df943c9feae0b9edaa270a2552bd
Brandon H Bodine <brandonhbodine@gmail.com>
2016-02-25 15:13:35 -0700

    Flusehd out the form componant

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 var React = require('react')
 var navbar = require('./navbar.jsx');
+var signup = require('./signup.jsx');
 
 var navbarElement = React.createElement(navbar);
+var signupElement = React.createElement(signup);
 
 ReactDOM.render(navbarElement, document.querySelector('.reactNavbarContainer'));
+ReactDOM.render(signupElement, document.querySelector('#signup'));
