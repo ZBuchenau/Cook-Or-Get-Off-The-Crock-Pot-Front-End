dd998a290d971b34cd2c852e13614c7d9751b375
KeenanHoffman <keenan.m.hoffman@gmail.com>
2016-02-24 18:35:27 -0700

    added a material navbar to react

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,8 @@
 var React = require('react')
 var nav = require('./nav.jsx');
+var navbar = require('./navbar.jsx');
 
+var navbarElement = React.createElement(navbar);
 var element = React.createElement(nav);
-ReactDOM.render(element, document.querySelector('.container'));
+ReactDOM.render(element, document.querySelector('.reactContainer'));
+ReactDOM.render(navbarElement, document.querySelector('.reactNavbarContainer'));
