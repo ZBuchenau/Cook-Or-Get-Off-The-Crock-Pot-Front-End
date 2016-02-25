442efa20a607bc67e4d38387b26e01357c5ae9e0
Brandon H Bodine <brandonhbodine@gmail.com>
2016-02-25 13:09:54 -0700

    Added some styles to the form for signup

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,6 @@
 var React = require('react')
-var nav = require('./nav.jsx');
 var navbar = require('./navbar.jsx');
 
 var navbarElement = React.createElement(navbar);
-var element = React.createElement(nav);
-ReactDOM.render(element, document.querySelector('.reactContainer'));
+
 ReactDOM.render(navbarElement, document.querySelector('.reactNavbarContainer'));
--- src/nav.jsx
+++ src/nav.jsx
@@ -1,13 +1,1 @@
 var React = require('react');
-
-module.exports = React.createClass({
-  render: function() {
-    return <nav>
-      <ul>
-        <a href="home.html"><li>Home</li></a>
-        <a href="about.html"><li>About</li></a>
-        <a href="helloWorld.html"><li>Hello World</li></a>
-      </ul>
-    </nav>
-  }
-});
