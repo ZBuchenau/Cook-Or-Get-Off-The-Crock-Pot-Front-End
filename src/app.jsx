1d8409bdc708d3688dcb1e68d3ae72c5fffaa8b8
Brandon H Bodine <brandonhbodine@gmail.com>
2016-02-26 11:49:17 -0700

    added a templated out router and auth package

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,31 @@
-var React = require('react')
-var navbar = require('./navbar.jsx');
-var signup = require('./signup.jsx');
-var recipes = require('./RecipeList.jsx');
+import React from 'react';
+import Router, {Route} from 'react-router';
+import AuthenticatedApp from './components/AuthenticatedApp'
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import RouterContainer from './services/RouterContainer';
+import LoginActions from './actions/LoginActions';
 
-var navbarElement = React.createElement(navbar);
-var signupElement = React.createElement(signup);
-var recipeElement = React.createElement(recipes);
+var routes = (
+  <Route handler={AuthenticatedApp}>
+    <Route name="login" handler={Login}/>
+    <Route name="signup" handler={Signup}/>
+    <Route name="home" path="/" handler={Home}/>
+    <Route name="quote" handler={Quote}/>
+    
+  </Route>
+);
 
+var router = Router.create({routes});
+RouterContainer.set(router);
 
-ReactDOM.render(recipeElement, document.querySelector('#recipes'));
-ReactDOM.render(navbarElement, document.querySelector('.reactNavbarContainer'));
-// ReactDOM.render(signupElement, document.querySelector('#signup'));
+let jwt = localStorage.getItem('jwt');
+if (jwt) {
+  LoginActions.loginUser(jwt);
+}
+
+router.run(function (Handler) {
+  React.render(<Handler />, document.getElementById('content'));
+});
---
+++ src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react/addons';
+import ReactMixin from 'react-mixin';
+import Auth from '../services/AuthService'
+
+export default class Login extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  login(e) {
+    e.preventDefault();
+    Auth.login(this.state.user, this.state.password)
+      .catch(function(err) {
+        alert("There's an error logging in");
+        console.log("Error logging in", err);
+      });
+  }
+
+  render() {
+    return (
+
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="text-center">Cook or get off the crock pot</h1>
+          <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p>
+            <a className="btn btn-primary btn-lg">Learn more</a>
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <h2>Login</h2>
+            <form className="well well-lg" action="index.html" method="post">
+              <div className="form-group label-floating">
+                <label for="" className="control-label">Username</label>
+                <input type="text" className="form-control" id="" placeholder="" valueLink={this.linkState('user')} />
+                <p className="help-block">Please enter your username</p>
+              </div>
+              <div className="form-group label-floating">
+                <label for="" className="control-label">Password</label>
+                <input type="text" className="form-control" id="password" ref="password" valueLink={this.linkState('password')} />
+                <p className="help-block"></p>
+              </div>
+              <button type="submit" classNameName="btn btn-default" onClick={this.login.bind(this)}>Login</button>
+            </form>
+          </div>
+          <div className="col-md-6">
+            <h2>Sign Up</h2>
+            <div className="well well-lg">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactMixin(Login.prototype, React.addons.LinkedStateMixin);
