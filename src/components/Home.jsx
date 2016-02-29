2b03e5da6f374fb9594c0ce63129af1f029660ec
Brandon H Bodine <brandonhbodine@gmail.com>
2016-02-29 11:52:20 -0700

    added greeting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import AuthenticatedComponent from './AuthenticatedComponent'
+import AuthenticatedComponent from './AuthenticatedComponent';
+import {Route, RouteHandler, Link} from 'react-router';
+import jwt_decode from 'jwt-decode';
 
 export default AuthenticatedComponent(class Home extends React.Component {
   render() {
-    return (<h1>Hello {this.props.user ? this.props.user.username : ''}</h1>);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-md-push-2">
+            <div className="well well-sm">
+              <h1 className="text-center">Hello {this.props.user ? this.props.user.username : ''}!</h1>
+              <p> Thanks for signing in! To get started with your weekly meal planning head over to <Link to="grocery">the recipe picker</Link> and pick what meals sound good to you.</p>
+              <p> Your number is {jwt_decode(this.props.jwt).phone} and will beuse to text you your shopping list</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
